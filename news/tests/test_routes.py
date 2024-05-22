91fc8b78701b3b9c5803225d57a72aea54e86b3e
Tania Belyashnikova <belyashnikova.tn@gmail.com>
2024-05-22 16:17:41 +0500

    add redirect test

--- news/tests/test_routes.py
+++ news/tests/test_routes.py
@@ -1,18 +1,29 @@
 from http import HTTPStatus
 
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
-from news.models import News
+from news.models import Comment, News
 
+
+User = get_user_model()
 
 class TestRoutes(TestCase):
 
     @classmethod
     def setUpTestData(cls):
         cls.news = News.objects.create(title='Name', text='Text')
+        cls.author = User.objects.create(username='Leo')
+        cls.reader = User.objects.create(username='Jusr reader')
+        cls.comment = Comment.objects.create(
+            news=cls.news,
+            author=cls.author,
+            text='simple text'
+        )
 
     def test_pages_availability(self):
+        """Checking free pages are avaliable"""
         urls = (
             ('news:home', None),
             ('news:detail', (self.news.id,)),
@@ -26,5 +37,38 @@ class TestRoutes(TestCase):
                 # и получаем адрес страницы для GET-запроса:
                 url = reverse(name, args=args)
                 response = self.client.get(url)
-                self.assertEqual(response.status_code, HTTPStatus.OK) 
+                self.assertEqual(response.status_code, HTTPStatus.OK)
 
+    def test_availability_for_comment_edit_and_delete(self):
+        """Checking only author of comment can edit/delete comment"""
+        users_statuses = (
+            (self.author, HTTPStatus.OK),
+            (self.reader, HTTPStatus.NOT_FOUND),
+        )
+        for user, status in users_statuses:
+            # Логиним пользователя в клиенте:
+            self.client.force_login(user)
+            # Для каждой пары "пользователь - ожидаемый ответ"
+            # перебираем имена тестируемых страниц:
+            for name in ('news:edit', 'news:delete'):  
+                with self.subTest(user=user, name=name):        
+                    url = reverse(name, args=(self.comment.id,))
+                    response = self.client.get(url)
+                    self.assertEqual(response.status_code, status)
+
+    def test_redirect_for_anonymous_client(self):
+        # Сохраняем адрес страницы логина:
+        login_url = reverse('users:login')
+        # В цикле перебираем имена страниц, с которых ожидаем редирект:
+        for name in ('news:edit', 'news:delete'):
+            with self.subTest(name=name):
+                # Получаем адрес страницы редактирования или удаления комментария:
+                url = reverse(name, args=(self.comment.id,))
+                # Получаем ожидаемый адрес страницы логина, 
+                # на который будет перенаправлен пользователь.
+                # Учитываем, что в адресе будет параметр next, в котором передаётся
+                # адрес страницы, с которой пользователь был переадресован.
+                redirect_url = f'{login_url}?next={url}'
+                response = self.client.get(url)
+                # Проверяем, что редирект приведёт именно на указанную ссылку.
+                self.assertRedirects(response, redirect_url)
