bd38cdbbfa992fd986fb44d6b6228fc823e50a77
Tania Belyashnikova <belyashnikova.tn@gmail.com>
2024-05-22 15:55:51 +0500

    add test_detail_page

--- news/tests/test_routes.py
+++ news/tests/test_routes.py
@@ -3,8 +3,14 @@ from http import HTTPStatus
 from django.test import TestCase
 from django.urls import reverse
 
+from news.models import News
+
 
 class TestRoutes(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.news = News.objects.create(title='Name', text='Text')
 
     def test_home_page(self):
         url = reverse('news:home')
@@ -12,4 +18,9 @@ class TestRoutes(TestCase):
         # и загружаем главную страницу.
         response = self.client.get(url)
         # Проверяем, что код ответа равен 200.
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_detail_page(self):
+        url = reverse('news:detail', kwargs={'pk': self.news.pk})
+        response = self.client.get(url)
         self.assertEqual(response.status_code, HTTPStatus.OK)
